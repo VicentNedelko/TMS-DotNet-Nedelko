939e28dec46618837e26bcb3a3ff25257c61ed08
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-12 20:06:42 +0300

    feat: add user input method;

--- src/Homework3/Program.cs
+++ src/Homework3/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace Homework3
+namespace DateTimeLesson
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime timeNow = DateTime.Now;
+            var userDate = GetUserDate();
+            Console.WriteLine($"Start DATE - {userDate.startDate}");
+            Console.WriteLine($"Finish DATE - {userDate.finishDate}");
+
+        }
+
+        private static (DateTime startDate, DateTime finishDate) GetUserDate()
+        {
+            DateTime startDate, finishDate;
+            Console.Write("Enter start Date : ");
+            string strDate = Console.ReadLine();
+            while (!DateTime.TryParse(strDate, out startDate))
+            {
+                Console.WriteLine("Error! Wrong Date entered.");
+                Console.Write("Enter start Date : ");
+                strDate = Console.ReadLine();
+            }
+            Console.Write("Enter finish date : ");
+            strDate = Console.ReadLine();
+            while (!DateTime.TryParse(strDate, out finishDate))
+            {
+                Console.WriteLine("Error! Wrong Date entered.");
+                Console.Write("Enter finish date : ");
+                strDate = Console.ReadLine();
+            }
+            var result = (startDate, finishDate);
+            return result;
         }
     }
 }
