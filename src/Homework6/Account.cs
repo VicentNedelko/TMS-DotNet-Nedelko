175eeadf8fb2d76465697981637e0a0d8e92c2aa
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-10-15 23:45:16 +0300

    feat: add solution for HW6

---
+++ src/Homework6/Account.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework6
+{
+    public class Account
+    {
+        private decimal _balance;
+        const decimal usdToByn = 2.57M;
+        const decimal euroToByn = 3.02M;
+        public decimal Balance { get; set; }
+        public Account(decimal sum)
+        {
+            Balance = sum;
+        }
+        public void AddMoney(decimal sumToAdd)
+        {
+            Balance += sumToAdd;
+        }
+        public void GetMoney(decimal sumToWithDraw)
+        {
+            Balance -= sumToWithDraw;
+        }
+        public void PrintActualBalance()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Actual Balance = {Balance}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void addInfo(decimal sum)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Your add {0:f2} BYN to your account.", sum);
+        }
+        public void addInfo(string str, decimal sum)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Your add {0:f2} BYN to your account.", sum);
+        }
+        public void withdrawInfo(string currency, decimal sum)
+        {
+            Console.WriteLine();
+            Console.WriteLine("You withdraw {0:f2} BYN.", sum);
+        }
+        public (string, decimal) ChooseCurrency()
+        {
+            (string, decimal) result;
+            Console.WriteLine("Choose currency : ");
+            Console.WriteLine("-------");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("BYN - [B]");
+            Console.WriteLine("USD - [U]");
+            Console.WriteLine("EURO - [E]");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-------");
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.B:
+                    Console.Clear();
+                    result = ("BYN", 1);
+                    break;
+                case ConsoleKey.U:
+                    Console.Clear();
+                    result = ("USD", usdToByn);
+                    break;
+                case ConsoleKey.E:
+                    Console.Clear();
+                    result =("EURO", euroToByn);
+                    break;
+                default:
+                    Console.Clear();
+                    result = ("X", 0);
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ src/Homework6/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+namespace Homework6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<decimal> Operation;
+            //Action<decimal> ShowInformation;
+            Action<string, decimal> ShowInformation;
+            bool goFuther = true;
+            Console.Write("Enter start sum [BYN] : ");
+            Account account = new Account(Decimal.Parse(Console.ReadLine()));
+            while(goFuther)
+            {
+                PrintUserMenu();
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.A:
+                        Console.Clear();
+                        Operation = account.AddMoney;
+                        ShowInformation = account.addInfo;
+                        Console.Write("Enter sum to add [BYN] : ");
+                        decimal sum = Decimal.Parse(Console.ReadLine());
+                        Operation.Invoke(sum);
+                        ShowInformation.Invoke("BYN",sum);
+                        break;
+                    case ConsoleKey.W:
+                        Console.Clear();
+                        Operation = account.GetMoney;
+                        ShowInformation = account.withdrawInfo;
+                        var userChoice = account.ChooseCurrency();
+                        Console.Write($"Enter sum to withdraw : [{userChoice.Item1}]");
+                        sum = Decimal.Parse(Console.ReadLine()) * userChoice.Item2;
+                        if (sum <= account.Balance)
+                        {
+                            Operation.Invoke(sum);
+                            ShowInformation(userChoice.Item1, sum);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error! Your sum is exceeding the balance.");
+                        }
+                        break;
+                    case ConsoleKey.D:
+                        Console.Clear();
+                        account.PrintActualBalance();
+                        break;
+                    case ConsoleKey.E:
+                        goFuther = false;
+                        break;
+                }
+
+            }
+        }
+        public static void PrintUserMenu()
+        {
+            Console.WriteLine("------");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Choose operation : ");
+            Console.WriteLine("Top up an Account - [A]");
+            Console.WriteLine("Withdrawal of funds - [W]");
+            Console.WriteLine("Display actual balance - [D]");
+            Console.WriteLine("Exit - [E]");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("------");
+        }
+    }
+}
